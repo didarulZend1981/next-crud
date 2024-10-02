92aa483693193214d79d498da8e50e55da1b042a
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-10-02 17:19:33 +0600

    dynamic single data details vai postman

---
+++ src/app/api/topics/[id]/route.js
@@ -0,0 +1,15 @@
+import connectMongoDB from "@/libs/mongodb";
+import Topic from "@/models/topic";
+import { NextResponse } from "next/server";
+
+
+
+
+
+
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const topic = await Topic.findOne({ _id: id });
+  return NextResponse.json({ topic }, { status: 200 });
+}
